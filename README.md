839d3843af3901b6b28635ac668c6eaed506896e
EasonChen <yschen1124@gmail.com>
2024-09-06 00:04:17 +0800

    -Updated README file, added project setup and running instructions -Added error handling and environment variable checking functions

--- README.md
+++ README.md
@@ -1,22 +1,18 @@
 1. copy .env.backend to .env and copy .flaskenv.backend to .flaskenv
 2. modify .env and .flaskenv
 
-   > FLASK_APP= your flask app file
-   > 
-   > API_KEY= your steVe API key
-   >
-   > DOCKER_NETWORK=
-   >
-   >> docker network ls
-   >> 
-   >> docker network inspect <network>
-   >>
-   >> find your steve network
-
-   > DOCKER_PORT= your steVe port
+   - FLASK_APP= your flask app file
+   - API_KEY= your steVe API key
+   - DOCKER_NETWORK_NAME= steve network name
+     > docker network ls
+     >
+     > docker network inspect <network>
+     >
+     > find your steve network
+   - DOCKER_APP_NETWORK= app network name
+   - DOCKER_PORT= your steVe port
 
 3. run code
-
 ```yaml
 docker-compose up --build
 ```
--- app.py
+++ app.py
@@ -2,28 +2,37 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # include CORS
 import requests
 import os
+from functools import wraps
 
 app = Flask(__name__, static_folder='static')
 CORS(app)  # allow all source
 
+API_KEY = os.getenv('API_KEY')
+DOCKER_NETWORK = os.getenv('DOCKER_APP_NETWORK')
+DOCKER_PORT = os.getenv('DOCKER_PORT')
+
+def check_required_env_vars(*required_vars):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            missing = [var for var in required_vars if not os.getenv(var)]
+            if missing:
+                return jsonify({'error': f'missing: {", ".join(missing)}'}), 400
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
 @app.route('/api/send-id', methods=['POST'])
+@check_required_env_vars('API_KEY', 'DOCKER_APP_NETWORK', 'DOCKER_PORT')
 def send_id():
     data = request.json
     id_tag = data.get('idTag')
-    API_KEY = os.getenv('API_KEY')
-    DOCKER_NETWORK = os.getenv('DOCKER_NETWORK', 'steve-steve-360-app-1')
-    DOCKER_PORT = os.getenv('DOCKER_PORT', 8180)
-    print(DOCKER_NETWORK, DOCKER_PORT)
-    if not API_KEY:
-        return jsonify({'error': 'API Key is required'}), 400
     if not id_tag:
         return jsonify({'error': 'ID Tag is required'}), 400
 
     # send post to SteVe API
     try:
         response = requests.post(f'http://{DOCKER_NETWORK}:{DOCKER_PORT}/steve/api/v1/ocppTags', json={'idTag': id_tag}, headers={'Content-Type': 'application/json', 'STEVE-API-KEY': API_KEY})
-        # response = requests.post('http://steve-steve-360-app-1:8180/steve/api/v1/ocppTags', json={'idTag': id_tag}, headers={'Content-Type': 'application/json', 'STEVE-API-KEY': 'a'})
-        # http://127.0.0.1:8180/steve/api/v1/ocppTags
         if response.status_code == 200:
             return jsonify(response.json())
         elif response.status_code == 201:
@@ -37,7 +46,4 @@ def send_id():
         return jsonify({'error': str(e)}), 500
 # more api ... @app.route('/api/...', methods=['POST'])
 if __name__ == '__main__':
-    # FLASK_RUN_PORT = os.getenv('FLASK_RUN_PORT')
-    # FLASK_RUN_HOST = os.getenv('FLASK_RUN_HOST')
-    # app.run(debug=True, host=FLASK_RUN_HOST, port=FLASK_RUN_PORT)
     pass
